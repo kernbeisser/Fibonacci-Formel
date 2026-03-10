36cbf9bf52f7311321b8c119652ae649c92bbe9b
kernbeisser <kernbeisser@tom-bonn.de>
2026-03-10 16:55:03 +0100

    Fibonacci Numbers & Colors

---
+++ README.md
@@ -0,0 +1,13 @@
+### A fun little programm with fibonacci numbers.
+
+
+There are different formulars to calculate fibonacci numbers: 
+
+
+- https://de.wikipedia.org/wiki/Fibonacci-Folge 
+- https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+
+The one i used is known as "Formel von Moivre-Binet".
+
+
---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+from colorama import Fore, Style, init
+import time
+
+
+def moivre_binet(n:int) ->int:
+    return 1/math.sqrt(5) * ((1 + math.sqrt(5)) / 2)**n - 1/math.sqrt(5) * ((1 - math.sqrt(5)) / 2)**n  # pyright: ignore[reportReturnType]
+
+
+def main():
+    
+    i:int = 0
+
+    init(autoreset=True) # colorama: Automatically resets color after each print
+    try:
+        for i in range(1500):
+            fn = moivre_binet(i)
+
+            print(f"\r{Fore.LIGHTGREEN_EX}{Style.BRIGHT}{i:4}: {Fore.YELLOW}{Style.BRIGHT}{fn:.0f}", end="", flush=True)
+            time.sleep(0.11)
+
+    except OverflowError:
+        print(f"\nJetzt sind Sie zu weit gegangen! {Fore.RED}{Style.BRIGHT}{i}")
+
+
+if __name__ == '__main__':
+    main()
+
